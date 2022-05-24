3089427658e0cfc47ee0a7aa7043f57f44ab2a58
vijaykumar <vijay1111mane@gmail.com>
2022-05-24 12:03:27 +0530

    commite

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,24 +1,52 @@
 <template>
+  <!-- click event start1 -->
+    <!-- <div>
+        <h1>Hello from Home component</h1>
+        // <h2>{{ data }}</h2>
+        // <h2>{{ msg }}</h2> 
+        <button v-on:click="hello('hello call')">Call Hello Function</button> 
+        <button v-on:mouseover="hello('hello call')">Call Hello Function</button>
+    </div> -->
+  <!-- click event end1 -->
+
+  <!-- Conditional Rendring start2 -->
   <div>
-    <h1>Hello from Home component</h1>
-    <!-- <h2>{{ data }}</h2>
-    <h2>{{ msg }}</h2> -->
-    <!-- <button v-on:click="hello('hello call')">Call Hello Function</button> -->
-    <button v-on:mouseover="hello('hello call')">Call Hello Function</button>
+    <h2 v-if="show">Conditional Rendering in vue js</h2>
+    <h2 v-else>Conditional Rendering else block</h2>
+    <button v-on:click="display">Toggle Element</button>
   </div>
+  <!-- Conditional Rendring End2 -->
 </template>
 <script>
 export default {
   name: "Home-page",
   props: {
     data: String,
-    msg: String
-  },methods:{
-      hello(data){
-        //   console.log("hello console called");
-        //   alert("hello console called");
-        alert(data)
-      }
-  }
+    msg: String,
+  },
+  //  Click event Start1
+
+  //   methods:{ //method property
+  //       hello(data){
+  //         //   console.log("hello console called");
+  //         //   alert("hello console called");
+  //         alert(data)
+  //       }
+  //   }
+
+  // Click event End1
+
+  // Conditional Rendring start2
+  data() {
+    return { show: true };
+  },
+  methods: {
+    display() {
+      // this.show=false //only hide
+      this.show = !this.show; // hide and show
+    },
+  },
+ // Conditional Rendring End2
+
 };
 </script>
